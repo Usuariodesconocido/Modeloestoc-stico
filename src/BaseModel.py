d010a0168be48c74c9b1f8a4f49337fff3068053
dcoello <dcpulido91@gmail.com>
2018-12-28 18:23:03 +0100

    conf casts

--- src/BaseModel.py
+++ src/BaseModel.py
@@ -11,19 +11,19 @@ class BaseModel:
         self.ARconf = ARconf
 
     def utility_function(self,
-                         conf
+                         conf,
                          cap,
                          capprima,
                          workS,
                          stocasticproc):
-        aux = (stocasticproc*pow(cap, conf["alfa"])*pow(workS, 1-conf["alfa"]) +
-               (1-conf[delta])*cap)/(1+conf["gamma"])*(1 + conf["n"])
+        aux = (stocasticproc*pow(cap, float(conf["alfa"]))*pow(workS, 1-float(conf["alfa"])) +
+               (1-float(conf["delta"]))*cap)/(1+float(conf["gamma"]))*(1 + float(conf["n"]))
         if aux < 0:
             return -10000
         else:
-            return math.log(stocasticproc*pow(cap, conf["alfa"])*pow(workS, 1-conf["alfa"]) -
-                            (1+conf["gamma"])*(1 + conf["n"])*capprima +
-                            (1-conf[delta])*cap)+conf[delta]*math.log(1-workS)
+            return math.log(stocasticproc*pow(cap, float(conf["alfa"]))*pow(workS, 1-float(conf["alfa"])) -
+                            (1+float(conf["gamma"]))*(1 + float(conf["n"]))*capprima +
+                            (1-float(conf["delta"]))*cap)+float(conf["delta"])*math.log(1-workS)
 
     def calculate_matrix_M(self, Mconf=None):
         if Mconf is None:
@@ -34,36 +34,36 @@ class BaseModel:
                 return
         else:
             cc = Mconf
-            
-        kvalues = linspace(
-            cc["Kmin"], cc["Kmax"], cc["Kresolution"])
-        hvalues = linspace(
-            cc["Hmin"], cc["Hmax"], cc["Hresolution"])
-        bvalues = linspace(
-            cc["Bmin"], cc["Bmax"], cc["Bresolution"])
-        toret = [[0 for x in range(cc["Kresolution"]*cc["Bresolution"])]
-                 for y in range(0, cc["Kresolution"]*cc["Hresolution"])]
+
+        print json.dumps(cc, indent=2)
+
+        kvalues = np.linspace(
+            float(cc["kmin"]), float(cc["kmax"]), float(cc["kresolution"]))
+        hvalues = np.linspace(
+            float(cc["hmin"]), float(cc["hmax"]), float(cc["hresolution"]))
+        bvalues = np.linspace(
+            float(cc["bmin"]), float(cc["bmax"]), float(cc["bresolution"]))
+        toret = [[0 for x in range(int(float(cc["kresolution"])*int(float(cc["bresolution"]))))]
+                 for y in range(0, int(float(cc["kresolution"])*int(float(cc["hresolution"]))))]
 
         countc = 0
         auxc = 0
-        for col in range(0, (cc["Kresolution"]-1)*(cc["Hresolution"]-1)):
-            if countc == cc["Kresolution"]-1:
+        for col in range(0, int(float(cc["kresolution"])-1)*int(float(cc["hresolution"])-1)):
+            if countc == float(cc["kresolution"])-1:
                 countc = 0
                 auxc = auxc + 1
             else:
                 countc = countc + 1
             countf = 0
             auxf = 0
-            for fila in range(0, (cc["Kresolution"]-1)*(cc["Bresolution"]-1)):
-                if countf == cc["Kresolution"]-1:
-                    print("reset")
+            for fila in range(0, int(float(cc["kresolution"])-1)*int(float(cc["bresolution"])-1)):
+                if countf == float(cc["kresolution"])-1:
                     countf = 0
                     auxf = auxf + 1
                 else:
-                    print("++")
                     countf = countf + 1
 
-                toret[col][fila] = utility_function(cc,
+                toret[col][fila] = self.utility_function(cc,
                                                     kvalues[countc],
                                                     kvalues[countf],
                                                     hvalues[auxf],
