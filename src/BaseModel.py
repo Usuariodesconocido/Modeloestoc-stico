dff50aa7ca8d1faf5b2764f2a6a739ba997deb2e
Usuariodesconocido <pomproblemas@gmail.com>
2018-12-28 17:59:50 +0100

    renamed parameters base model

--- src/BaseModel.py
+++ src/BaseModel.py
@@ -10,35 +10,35 @@ class BaseModel:
     def utility_function(self,
                          cap,
                          capprima,
-                         tres,
-                         cuatro):
-        aux = (cuatro*pow(uno, alfa)*pow(tres, 1-alfa) +
-               (1-delta)*uno)/(1+gamma)*(1 + n)
+                         workS,
+                         stocasticproc):
+        aux = (stocasticproc*pow(cap, self.conf["alfa"])*pow(workS, 1-self.conf["alfa"]) +
+               (1-self.conf[delta])*cap)/(1+self.conf["gamma"])*(1 + self.conf["n"])
         if aux < 0:
             return -10000
         else:
-            return np.log(cuatro*pow(uno, alfa)*pow(tres, 1-alfa) -
-                          (1+gamma)*(1 + n)*dos +
-                          (1-delta)*uno)+delta*np.log(1-tres)
+            return math.log(stocasticproc*pow(cap, self.conf["alfa"])*pow(workS, 1-self.conf["alfa"]) -
+                          (1+self.conf["gamma"])*(1 + self.conf["n"])*capprima +
+                          (1-self.conf[delta])*cap)+self.conf[delta]*math.log(1-workS)
 
     def calculate_matrix_M(self):
-        kvalues = linspace(kmin, kmax, lado)
-        hvalues = linspace(Hmin, Hmax, colit)
-        tvalues = linspace(Bmin, Bmax, fiit)
-        toret = [[0 for x in range(lado*fiit)] for y in range(0, lado*colit)]
+        kvalues = linspace(self.conf["Kmin"], self.conf["Kmax"], self.conf["Kresolution"])
+        hvalues = linspace(self.conf["Hmin"], self.conf["Hmax"], self.conf["Hresolution"])
+        bvalues = linspace(self.conf["Bmin"], self.conf["Bmax"], self.conf["Bresolution"])
+        toret = [[0 for x in range(self.conf["Kresolution"]*self.conf["Bresolution"])] for y in range(0, self.conf["Kresolution"]*self.conf["Hresolution"])]
 
         countc = 0
         auxc = 0
-        for col in range(0, (lado-1)*(colit-1)):
-            if countc == lado-1:
+        for col in range(0, (self.conf["Kresolution"]-1)*(self.conf["Hresolution"]-1)):
+            if countc == self.conf["Kresolution"]-1:
                 countc = 0
                 auxc = auxc + 1
             else:
                 countc = countc + 1
             countf = 0
             auxf = 0
-            for fila in range(0, (lado-1)*(fiit-1)):
-                if countf == lado-1:
+            for fila in range(0, (self.conf["Kresolution"]-1)*(self.conf["Bresolution"]-1)):
+                if countf == self.conf["Kresolution"]-1:
                     print("reset")
                     countf = 0
                     auxf = auxf + 1
@@ -47,7 +47,7 @@ class BaseModel:
                     countf = countf + 1
               
                
-                toret[col][fila] = W(kvalues[countc], kvalues[countf], hvalues[auxf], tvalues[auxc])
+                toret[col][fila] = W(kvalues[countc], kvalues[countf], hvalues[auxf], bvalues[auxc])
 
         return toret
 
