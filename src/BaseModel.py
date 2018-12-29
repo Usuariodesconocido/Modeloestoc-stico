4c0b2a505f1bd2c42d219be583ac1f9db846a581
Usuariodesconocido <pomproblemas@gmail.com>
2018-12-29 20:42:36 +0100

    autorregresive proess uptdated to stacionary values, config.txt modified

--- src/BaseModel.py
+++ src/BaseModel.py
@@ -5,6 +5,7 @@ from decimal import Decimal
 from scipy import special
 import logging
 
+
 class BaseModel:
     def __init__(self,
                  Mconf=None,
@@ -20,25 +21,37 @@ class BaseModel:
                          stocasticproc):
         aux = (stocasticproc*pow(cap, float(conf["alfa"]))*pow(workS, 1-float(conf["alfa"])) +
                (1-float(conf["delta"]))*cap)/(1+float(conf["gamma"]))*(1 + float(conf["n"]))
+
         if aux < capprima:
             return -10000
         else:
-            return math.log1p(Decimal(stocasticproc*pow(cap, float(conf["alfa"]))*pow(workS, 1-float(conf["alfa"])) -
-                                      (1+float(conf["gamma"]))*(1 + float(conf["n"]))*capprima +
-                                      (1-float(conf["delta"]))*cap))+(float(conf["delta"])*math.log1p(float(1-workS)))
+            values = math.log1p(stocasticproc*pow(cap, float(conf["alfa"]))*pow(workS, 1-float(conf["alfa"])) -
+                                (1+float(conf["gamma"]))*(1 + float(conf["n"]))*capprima +
+                                (1-float(conf["delta"]))*cap)+(float(conf["delta"])*math.log1p(float(1-workS)))
+
+            return values
 
     def calculate_bvalues(self, conf):
-        m=3
+
+        th = (pow(float(conf["ke"]), -1*float(conf["alfa"]))
+              * (pow(float(conf["he"]), float(conf["alfa"])-1)))
+
+        mu = (1-float(conf["rho"]))*th
+        sig = 2*mu
+
         s = np.zeros((int(conf["bresolution"]), 1))
+
         pi = np.zeros((int(conf["bresolution"]), int(conf["bresolution"])))
-        s[0, :] = float(conf["mu"])/(1-float(conf["rho"]))-m * \
-            math.sqrt(pow(float(conf["sig"]), 2) /
+
+        s[0, :] = mu/(1-float(conf["rho"]))-int(conf["m"]) * \
+            math.sqrt(pow(sig, 2) /
+
                       (1-(pow(float(conf["rho"]), 2))))
-        s[int(conf["bresolution"])-1, :] = float(conf["mu"])/(1-float(conf["rho"])) + \
-            m*math.sqrt(pow(float(conf["sig"]), 2) /
-                        (1-(pow(float(conf["rho"]), 2))))
-        step = (s[int(conf["bresolution"])-1, :] - s[0, :]) / \
-            (int(conf["bresolution"])-1)
+        s[int(conf["bresolution"])-1, :] = mu/(1-float(conf["rho"])) + \
+            int(conf["m"])*math.sqrt(pow(sig, 2) /
+                                     (1-(pow(float(conf["rho"]), 2))))
+
+        step = (s[int(conf["bresolution"])-1, :] - s[0, :]) / (int(conf["bresolution"])-1)
 
         for i in range(1, int(conf["bresolution"])-1):
             s[i, :] = s[i-1, :]+step
@@ -46,14 +59,14 @@ class BaseModel:
         for j in range(0, int(conf["bresolution"])-1):
             for k in range(0, int(conf["bresolution"])-1):
                 if k == 0:
-                    pi[j][k] = 0.5*special.erfc((-1*(s[0, :]-float(conf["mu"])-float(
-                        conf["rho"]*s[j, :])+step/2)/float(conf["sig"]))/math.sqrt(2))
+                    pi[j][k] = 0.5*special.erfc((-1*(s[0, :]-mu-float(
+                        conf["rho"])*s[j, :]+step/2)/sig)/math.sqrt(2))
                 elif k == int(conf["bresolution"]):
-                    pi[j][k] = 1-(0.5*special.erfc((-(s[int(conf["bresolution"])-1, :]-float(
-                        conf["mu"])-float(conf["rho"]*s[j, :])+step/2)/float(conf["sig"]))/math.sqrt(2)))
+                    pi[j][k] = 1-(0.5*special.erfc((-(s[int(conf["bresolution"])-1, :] -
+                                                      mu-float(conf["rho"]*s[j, :])+step/2)/sig)/math.sqrt(2)))
                 else:
-                    pi[j][k] = 0.5*special.erfc((-(s[k, :]-float(conf["mu"])-float(conf["rho"]*s[j, :])+step/2)/float(conf["sig"]))/math.sqrt(
-                        2))-0.5*special.erfc((-(s[k, :]-float(conf["mu"])-float(conf["rho"]*s[j, :])+step/2)/float(conf["sig"]))/math.sqrt(2))
+                    pi[j][k] = 0.5*special.erfc((-1*(s[k, :]-mu-float(conf["rho"])*s[j, :]+step/2)/sig)/math.sqrt(
+                        2))-0.5*special.erfc((-(s[k, :]-mu-float(conf["rho"])*s[j, :]+step/2)/sig)/math.sqrt(2))
 
         return s
 
@@ -85,31 +98,38 @@ class BaseModel:
 
         bvalues = self.calculate_bvalues(ca)
 
+        print(kvalues)
+        print(hvalues)
+        print(bvalues)
+
         toret = [[0 for x in range(int(float(cc["kresolution"])*int(float(cc["bresolution"]))))]
                  for y in range(0, int(float(cc["kresolution"])*int(float(cc["hresolution"]))))]
 
         countc = 0
         auxc = 0
-        for col in range(0, int(float(cc["kresolution"])-1)*int(float(cc["hresolution"])-1)):
+        for col in range(0, int(float(cc["kresolution"])-1)*int(float(cc["bresolution"])-1)):
             if countc == float(cc["kresolution"])-1:
                 countc = 0
                 auxc = auxc + 1
             else:
                 countc = countc + 1
+
             countf = 0
             auxf = 0
-            for fila in range(0, int(float(cc["kresolution"])-1)*int(float(cc["bresolution"])-1)):
+            for fila in range(0, int(float(cc["kresolution"])-1)*int(float(cc["hresolution"])-1)):
                 if countf == float(cc["kresolution"])-1:
                     countf = 0
                     auxf = auxf + 1
                 else:
                     countf = countf + 1
 
-                toret[col][fila] = self.utility_function(cc,
-                                                         kvalues[countc],
-                                                         kvalues[countf],
-                                                         hvalues[auxf],
-                                                         bvalues[auxc])
+                b = self.utility_function(cc,
+                                          kvalues[countc],
+                                          kvalues[countf],
+                                          hvalues[auxf],
+                                          bvalues[auxc])
+
+                toret[col][fila] = b
 
         return toret
 
